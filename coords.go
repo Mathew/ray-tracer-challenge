40b066c19a53cd3e4dc9184ba7696f0a3bc04ce6
Mathew Taylor <mat@mathewtaylor.co.uk>
2019-11-14 22:36:23 +0000

    Ability to add coords.

--- coords.go
+++ coords.go
@@ -1,35 +1,33 @@
 package ray_tracer_challenge
 
-type Coords struct {
+import "errors"
+
+type Coord struct {
 	X float64
 	Y float64
 	Z float64
+	W int8
 }
 
-type Vector struct {
-	coords Coords
-	w      int8
-}
 
-type Point struct {
-	coords Coords
-	w      int8
-}
+var TwoPointsError = errors.New("cannot add two points")
 
-func NewCoords(x, y, z float64) Coords {
-	return Coords{x, y, z}
-}
-
-func NewVector(x, y, z float64) Vector {
-	return Vector{
-		coords: NewCoords(x, y, z),
-		w:      0,
+func (c Coord) add (other Coord) (Coord, error) {
+	if c.W + other.W == 2 {
+		return Coord{}, TwoPointsError
 	}
+
+	return NewCoord(c.X + other.X, c.Y + other.Y, c.Z + other.Z, c.W + other.W), nil
 }
 
-func NewPoint(x, y, z float64) Point {
-	return Point{
-		coords: NewCoords(x, y, z),
-		w:      1,
-	}
+func NewCoord(x, y, z float64, w int8) Coord {
+	return Coord{x, y, z, w}
+}
+
+func NewVector(x, y, z float64) Coord {
+	return NewCoord(x, y, z, 0)
+}
+
+func NewPoint(x, y, z float64) Coord {
+	return NewCoord(x, y, z, 1)
 }
--- coords_test.go
+++ coords_test.go
@@ -1,6 +1,7 @@
 package ray_tracer_challenge
 
 import (
+	"errors"
 	"github.com/mathew/ray-tracer-challenge/asserts"
 	"testing"
 )
@@ -8,17 +9,40 @@ import (
 func TestNewPoint(t *testing.T) {
 	p := NewPoint(1, 2, 3)
 
-	asserts.Equals(t, 1.0, p.coords.X)
-	asserts.Equals(t, 2.0, p.coords.Y)
-	asserts.Equals(t, 3.0, p.coords.Z)
-	asserts.Equals(t, int8(1), p.w)
+	asserts.Equals(t, 1.0, p.X)
+	asserts.Equals(t, 2.0, p.Y)
+	asserts.Equals(t, 3.0, p.Z)
+	asserts.Equals(t, int8(1), p.W)
 }
 
 func TestNewVector(t *testing.T) {
 	p := NewVector(1, 2, 3)
 
-	asserts.Equals(t, 1.0, p.coords.X)
-	asserts.Equals(t, 2.0, p.coords.Y)
-	asserts.Equals(t, 3.0, p.coords.Z)
-	asserts.Equals(t, int8(0), p.w)
+	asserts.Equals(t, 1.0, p.X)
+	asserts.Equals(t, 2.0, p.Y)
+	asserts.Equals(t, 3.0, p.Z)
+	asserts.Equals(t, int8(0), p.W)
+}
+
+func TestCompareVectors(t *testing.T) {
+	a := NewVector(1, 2, 3)
+	b := NewVector(1, 2, 3)
+	c := NewVector(1, 2, 4)
+
+	asserts.Assert(t, a == b, "A does not equal B")
+	asserts.Assert(t, a != c, "A does equal C")
+}
+
+func TestAddCoords(t *testing.T) {
+	v := NewVector(1, 2, 3)
+	p := NewPoint(1, 2, 3)
+
+	add, _ := v.add(p)
+
+	asserts.Equals(t, 2.0, add.X)
+	asserts.Equals(t, 4.0, add.Y)
+	asserts.Equals(t, 6.0, add.Z)
+
+	_, err := p.add(p)
+	asserts.Assert(t, errors.Is(err, TwoPointsError), "TwoPointsError not raised")
 }
